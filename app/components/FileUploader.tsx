1ecd1be82ce8625277138bc0548acd1d5b687cfb
harshal chabukswar <harshal.c@seamlessautomations.com>
2025-12-19 12:27:13 +0530

    building a form compo

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone';
+
+
+const FileUploader = () => {
+    const [file, setFile] = useState();
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        const file = acceptedFiles[0] || null
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+    return (
+        <div className='w-full gradiet-border'>
+            <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <div className="space-y-4 cursor-pointer">
+                    <div className="mx-auto w-16 h-16 flex items-center justify-center">
+                        <img src="/icons/info.svg" alt="upload" className='size-20' />
+                    </div>
+                    {file ? (
+                        <div>
+
+                        </div>
+                    ) : (
+                        <div>
+                            <p className="text-lg text-gray-500">
+                                <span className="font-semibold">
+                                    Click to upload
+                                </span> or drag and drop
+                            </p>
+                            <p className="text-lg text-gray-500">PDF (max 20MB)</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default FileUploader
