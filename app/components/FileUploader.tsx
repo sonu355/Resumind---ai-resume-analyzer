9ce7f5d91a16ac771618346f8f2d02f7c927c75c
harshal chabukswar <harshal.c@seamlessautomations.com>
2025-12-19 18:44:27 +0530

    form input values extracted

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,34 +1,66 @@
 import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone';
+import { formatSize } from '~/lib/utils';
 
+interface fileUploaderProps {
+    onFileSelect?: (file: File | null) => void;
+}
 
-const FileUploader = () => {
-    const [file, setFile] = useState();
+const FileUploader = ({onFileSelect}: fileUploaderProps) => {
+    // const [file, setFile] = useState();
+
     const onDrop = useCallback((acceptedFiles: File[]) => {
         const file = acceptedFiles[0] || null
-    }, [])
-    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+        onFileSelect?.(file)
+    }, [onFileSelect])
+
+    const maxFileSize = 20 * 1024 * 1024; // 20MB
+
+    const {getRootProps, getInputProps, isDragActive, acceptedFiles} = useDropzone({
+        onDrop,
+        multiple: false,
+        accept: { 'application/pdf': ['.pdf']},
+        maxSize: maxFileSize
+    })
+
+    const file = acceptedFiles[0] || null
 
     return (
         <div className='w-full gradiet-border'>
             <div {...getRootProps()}>
                 <input {...getInputProps()} />
                 <div className="space-y-4 cursor-pointer">
-                    <div className="mx-auto w-16 h-16 flex items-center justify-center">
-                        <img src="/icons/info.svg" alt="upload" className='size-20' />
-                    </div>
+                    
                     {file ? (
-                        <div>
-
+                        <div className='uploader-selected-file' onClick={(e) => e.stopPropagation()}> 
+                            <img src="/images/pdf.png" alt="pdf" className='size-10' />
+                            <div className='items-center flex space-x-3'>
+                                <div>
+                                    <p className='text-lg text-gray-700 font-medium truncate max-w-xs'>
+                                        {file.name}
+                                    </p>
+                                    <p className='text-sm text-gray-500'>
+                                        {formatSize(file.size)}
+                                    </p>
+                                </div>                            
+                            </div>
+                            <button className='p-2 cross-pointer' onClick={(e) => 
+                                onFileSelect?.(null)
+                            }>
+                                <img src="/icons/cross.svg" alt="remove" className='w-4 h-4' />
+                            </button>
                         </div>
                     ) : (
                         <div>
+                            <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                                <img src="/icons/info.svg" alt="upload" className='size-20' />
+                            </div>
                             <p className="text-lg text-gray-500">
                                 <span className="font-semibold">
                                     Click to upload
                                 </span> or drag and drop
                             </p>
-                            <p className="text-lg text-gray-500">PDF (max 20MB)</p>
+                            <p className="text-lg text-gray-500">PDF (max {formatSize(maxFileSize)})</p>
                         </div>
                     )}
                 </div>
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -6,8 +6,25 @@ import FileUploader from '~/components/FileUploader';
 const upload = () => {
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('')
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const [file, setFile] = useState<File | null>(null);
 
+    const handleFileSelect = (file: File | null) => {
+        setFile(file)
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget.closest('form');
+        if(!form) return;
+        const formData = new FormData(form);
+
+        const companyName = formData.get('company-name');
+        const jobTitle = formData.get('job-title');
+        const jobDescription = formData.get('job-description');
+
+        console.log({
+            companyName, jobTitle, jobDescription, file
+        })
     }
   
     return (
@@ -40,7 +57,7 @@ const upload = () => {
                             </div>
                             <div className="form-div">
                                 <label htmlFor="uploader">Uploader</label>
-                                <FileUploader />
+                                <FileUploader onFileSelect={handleFileSelect}/>
                             </div>
                             <button className="primary-button" type='submit'>Analyze Resume</button>
                         </form>
